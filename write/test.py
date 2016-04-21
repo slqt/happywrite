13cfc4f790299a8ffe1f2631e700ed3b58e02817
slqt <hzqt210@gmail.com>
2016-04-21 11:30:09 +0800

    Create test.py

---
+++ write/test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on 2016-04-07
+@author: slqt
+'''
+import commands
+import platform
+
+def getInformation():
+    grains={}
+    try:
+        grains['resCentOS'] = grains.update(
+            {'resName': _resName()})
+        return grains['resCentOS']
+    except Exception,e:
+        logging.debug(e)
+        return {'error':'Error had happened!'}
+        
+def _resName():
+    '''
+    get resource name for minions
+    '''
+    try:
+        resName_info = platform.node()
+        return resName_info
+    except Exception,e:
+        logging.info(e)
+        resName_info = commands.getstatusoutput('hostname')
+        return resName_info
+    except Exception, e:
+        logging.debug(e)
+        return ""
+        
+if __name__=="__main__":
+    a=getInformation()
+    print a
